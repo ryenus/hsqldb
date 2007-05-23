8c2632d3c819c9e08f99f107bacbd3fdbc381750
boucherb <boucherb@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2007-05-23 06:10:50 +0000

    Just some flaky 'ease of use' batch files
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@113 7c7dc5f5-a22d-0410-a3af-b41755a11667

---
+++ build/setenv.cmd
@@ -0,0 +1,8 @@
+REM basic environment setup
+REM change as needed
+
+set JAVA_BASE=E:\java
+set JAVA_HOME=%JAVA_BASE%\vm\sun\jdk-1.6.0_01
+set ANT_HOME=%JAVA_BASE%\lib\ant
+set ANT_OPTS=-Xmx512m
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
