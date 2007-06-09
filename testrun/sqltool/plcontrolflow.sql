49af2b55f06adc7a14260eb83c51126d8f7384fa
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2007-06-09 14:35:41 +0000

    Added positive and negative tests for deep IF nesting
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@205 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- testrun/sqltool/plcontrolflow.sql
+++ testrun/sqltool/plcontrolflow.sql
@@ -30,12 +30,116 @@
     * end if
     * L11 = true
 * end if
-*if (! L1)
+*if (! *L1)
     \q Pre-nest failure
 *end if
-*if (! L2)
+*if (! *L2)
     \q Inside-nest failure
 *end if
-*if (! L11)
+*if (! *L11)
     \q Post-nest failure
+*end if
+* L1 =
+* L2 =
+* L11 =
+* if (1)
+    * L1 = true
+    * if (2)
+        * L2 = true
+    * end if
+    * L11 = true
+* end if
+*if (! *L1)
+    \q Pre-nest failure
+*end if
+*if (! *L2)
+    \q Inside-nest failure
+*end if
+*if (! *L11)
+    \q Post-nest failure
+*end if
+/* Test deep nesting of IFs, including negatives. */
+* if (1)
+    * L1 = true
+    * if (0)
+        * N2 = true
+    * end if
+    * if (2)
+        * L2 = true
+        * if (3)
+            * L3 = true
+            * if (4)
+                * L4 = true
+                * if (0)
+                    * N5 = true
+                * end if
+                * if (5)
+                    * L5 = true
+                    * if (0)
+                        * N6 = true
+                    * end if
+                    * if (6)
+                        * L6 = true
+                        * if (0)
+                            * N7 = true
+                        * end if
+                    * end if
+                    * L51 = true
+                * end if
+            * end if
+            * if (0)
+                * N4 = true
+            * end if
+            * L31 = true
+        * end if
+        * if (0)
+            * N3 = true
+        * end if
+    * end if
+    * L11 = true
+* end if
+*if (! *L1)
+    \q Pre-deep-nest failure 1
+*end if
+*if (! *L2)
+    \q Inside-deep-nest failure 2
+*end if
+*if (! *L11)
+    \q Post-deep-nest failure 11
+*end if
+*if (! *L3)
+    \q Pre-deep-nest failure 3
+*end if
+*if (! *L4)
+    \q Inside-deep-nest failure 4
+*end if
+*if (! *L31)
+    \q Post-deep-nest failure 31
+*end if
+*if (! *L5)
+    \q Pre-deep-nest failure 5
+*end if
+*if (! *L6)
+    \q Inside-deep-nest failure 6
+*end if
+*if (! *L51)
+    \q Post-deep-nest failure 51
+*end if
+*if (*N2)
+    \q Negative deep-nest failure 2
+*end if
+*if (*N3)
+    \q Negative deep-nest failure 3
+*end if
+*if (*N4)
+    \q Negative deep-nest failure 4
+*end if
+*if (*N5)
+    \q Negative deep-nest failure 5
+*end if
+*if (*N6)
+    \q Negative deep-nest failure 6
+*end if
+*if (*N7)
+    \q Negative deep-nest failure 7
 *end if
