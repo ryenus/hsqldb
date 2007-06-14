9d98c6db9794844f32f7bf786824b5c2f8fb0c90
Brittany Balliett <boucherb@users.sf.net>
2007-06-14 12:17:20 +0000

    Do not specify JVM micro-version specific path.
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@255 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/build/setenv.cmd
+++ trunk/build/setenv.cmd
@@ -2,7 +2,7 @@ REM basic environment setup
 REM change as needed
 
 set JAVA_BASE=E:\java
-set JAVA_HOME=%JAVA_BASE%\vm\sun\jdk-1.6.0_01
+set JAVA_HOME=%JAVA_BASE%\vm\sun\1.6.0
 set ANT_HOME=%JAVA_BASE%\lib\ant
 set ANT_OPTS=-Xmx512m
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
