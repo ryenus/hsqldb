e94b106240b5770663bbba877d268bc66953808e
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2007-09-05 02:04:36 +0000

    Pulled in Blaine's 08/2007 updates to CVS hsqldb-dev
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@361 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/util/GridSwing.java
+++ src/org/hsqldb/util/GridSwing.java
@@ -117,6 +117,16 @@ class GridSwing extends AbstractTableModel {
             Object o = getValueAt(0, i);
 
             if (o != null) {
+                if ((o instanceof java.sql.Timestamp)
+                    || (o instanceof java.sql.Time)) {
+                    // This is a workaround for JTable's lack of a default
+                    // renderer that displays times.
+                    // Without this workaround, Timestamps (and similar
+                    // classes) will be displayed as dates without times,
+                    // since JTable will match these classes to their
+                    // java.util.Date superclass.
+                    return Object.class;  // renderer will draw .toString().
+                }
                 return o.getClass();
             }
         }
