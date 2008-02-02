d5f937683933503b1f542d416e3b1b98f44bd46e
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2008-02-02 14:12:43 +0000

    Fixed typos which caused a few failures to not be detected
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@377 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- testrun/sqltool/lastval.sql
+++ testrun/sqltool/lastval.sql
@@ -7,27 +7,27 @@
 CREATE TABLE t (i INT);
 
 * if (*? != 0)
-    \p ? variable not capturing CREATE TABLE return value
+    \q ? variable not capturing CREATE TABLE return value
 * end if
 
 INSERT INTO t values (21);
 * if (*? != 1)
-    \p ? variable not capturing INSERT return value
+    \q ? variable not capturing INSERT return value
 * end if
 
 INSERT INTO t values (10);
 * if (*? != 1)
-    \p ? variable not capturing INSERT return value
+    \q ? variable not capturing INSERT return value
 * end if
 
 INSERT INTO t values (43);
 * if (*? != 1)
-    \p ? variable not capturing INSERT return value
+    \q ? variable not capturing INSERT return value
 * end if
 
 SELECT * FROM t ORDER BY i DESC;
 * if (*? != 10)
-    \p ? variable not capturing last fetched value
+    \q ? variable not capturing last fetched value
 * end if
 
 \p echo some stuff
@@ -35,8 +35,8 @@ SELECT * FROM t ORDER BY i DESC;
 * list
 
 * if (*? != 10)
-    \p ? value not retained after special commands
+    \q ? value not retained after special commands
 * end if
 * if (*{?} != 10)
-    \p ? value not dereferenced with {} usage
+    \q ? value not dereferenced with {} usage
 * end if
