5d05802ae40e4a5aad3b29039da421d2f34cf379
boucherb <boucherb@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2009-01-04 00:05:36 +0000

    Add @Override attributes.
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@713 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- test-src/org/hsqldb/jdbc/UtilTest.java
+++ test-src/org/hsqldb/jdbc/UtilTest.java
@@ -322,10 +322,12 @@ public class UtilTest extends BaseTestCase {
         m_vendorCode = vendorCode;
     }
 
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
     }
 
+    @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
@@ -424,6 +426,7 @@ public class UtilTest extends BaseTestCase {
 
     private int m_vendorCode;
 
+    @Override
     protected void runTest() throws Throwable {
 
         println(getName());
