8c48c688dc1cfdbe2cd83f8a41373fa86aad67fa
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2009-07-09 13:01:04 +0000

    core code updates
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@3031 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/rowio/RowInputTextLog.java
+++ src/org/hsqldb/rowio/RowInputTextLog.java
@@ -328,7 +328,18 @@ implements RowInputInterface {
 
     protected Boolean readBoole() throws IOException {
 
-        readField();
+        readFieldPrefix();
+        scanner.scanNext();
+
+        String token = scanner.getString();
+
+        value = null;
+
+        if (token.equalsIgnoreCase(Tokens.T_TRUE)) {
+            value = Boolean.TRUE;
+        } else if (token.equalsIgnoreCase(Tokens.T_FALSE)) {
+            value = Boolean.FALSE;
+        }
 
         return (Boolean) value;
     }
