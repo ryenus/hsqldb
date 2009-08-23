18b6183aaae14d618cf7c702b40a28ebba011484
Brittany Balliett <boucherb@users.sf.net>
2009-08-23 23:36:54 +0000

    Updated test suite to reflect 1.9.0 RC status.
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3125 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/build/setenv.cmd
+++ trunk/build/setenv.cmd
@@ -1,8 +1,7 @@
 REM basic environment setup
 REM change as needed
 
-set JAVA_BASE=E:\java
-set JAVA_HOME=%JAVA_BASE%\vm\sun\1.6.0
-set ANT_HOME=%JAVA_BASE%\lib\ant
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0
+set ANT_HOME=C:\java\lib\ant
 set ANT_OPTS=-Xmx512m
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
