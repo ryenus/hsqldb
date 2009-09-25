b667e418096f73c5cc93a27d884f89a823bcbafd
Fred Toussi <fredt@users.sf.net>
2009-09-25 11:16:42 +0000

    core code updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3220 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/types/DateTimeType.java
+++ trunk/src/org/hsqldb/types/DateTimeType.java
@@ -1331,6 +1331,24 @@ public final class DateTimeType extends DTIType {
                && other.endPartIndex <= endPartIndex;
     }
 
+    public int getSqlDateTimeSub() {
+
+        switch (typeCode) {
+
+            case Types.SQL_DATE :
+                return 1;
+
+            case Types.SQL_TIME :
+                return 2;
+
+            case Types.SQL_TIMESTAMP :
+                return 3;
+
+            default :
+                return 0;
+        }
+    }
+
     public static Boolean overlaps(Session session, Object[] a, Type[] ta,
                                    Object[] b, Type[] tb) {
 
