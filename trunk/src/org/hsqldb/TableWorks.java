0ddf720068c305639c662fb1b6052b0b130f11d0
Fred Toussi <fredt@users.sf.net>
2010-02-26 18:05:06 +0000

    license date updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3481 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/TableWorks.java
+++ trunk/src/org/hsqldb/TableWorks.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001-2009, The HSQL Development Group
+/* Copyright (c) 2001-2010, The HSQL Development Group
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
