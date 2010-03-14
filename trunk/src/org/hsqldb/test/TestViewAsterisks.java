263c2b4d24c67c6053a876d17bdea1373947a6fd
Fred Toussi <fredt@users.sf.net>
2010-03-14 14:36:49 +0000

    test updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3526 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/test/TestViewAsterisks.java
+++ trunk/src/org/hsqldb/test/TestViewAsterisks.java
@@ -390,20 +390,20 @@ public class TestViewAsterisks extends TestBase {
 
         // ................................................................
         checkViewTranslationAndContent(
-            "C1", null, "SELECT *, A AS \"a2\" FROM ABC",
-            "SELECT PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C ,A AS\"a2\" FROM PUBLIC.ABC",
+            "C1", null, "SELECT * AS \"a2\" FROM ABC",
+            "SELECT PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C AS\"a2\" FROM PUBLIC.ABC",
             new Object[][] {
             new Object[] {
-                new Integer(1), "a", "b", "c", "a"
+                new Integer(1), "a", "b", "c"
             }, new Object[] {
-                new Integer(2), "d", "e", "f", "d"
+                new Integer(2), "d", "e", "f"
             }
         });
 
         // ................................................................
         checkViewTranslationAndContent(
-            "C2", null, "SELECT B AS \"b2\", * FROM ABC",
-            "SELECT B AS\"b2\" , PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C FROM PUBLIC.ABC",
+            "C2", null, "SELECT B AS \"b2\", ABC.* FROM ABC",
+            "SELECT B AS\"b2\" , PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C  FROM PUBLIC.ABC",
             new Object[][] {
             new Object[] {
                 "b", new Integer(1), "a", "b", "c"
