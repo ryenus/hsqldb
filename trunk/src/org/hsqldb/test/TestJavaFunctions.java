7462d64649d11a351e18ba92ed43c080f748c5a3
Fred Toussi <fredt@users.sf.net>
2010-03-19 15:17:24 +0000

    core code updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3544 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/test/TestJavaFunctions.java
+++ trunk/src/org/hsqldb/test/TestJavaFunctions.java
@@ -34,6 +34,18 @@ package org.hsqldb.test;
 import java.sql.*;
 import java.sql.SQLException;
 
+import org.hsqldb.navigator.RowSetNavigatorClient;
+import org.hsqldb.navigator.RowSetNavigator;
+import org.hsqldb.result.ResultMetaData;
+import org.hsqldb.types.Type;
+import org.hsqldb.result.Result;
+import org.hsqldb.jdbc.JDBCResultSet;
+import org.hsqldb.jdbc.JDBCConnection;
+
+import java.math.BigInteger;
+
+import org.hsqldb.types.BinaryData;
+
 public class TestJavaFunctions extends TestBase {
 
     public TestJavaFunctions() {
@@ -56,7 +68,8 @@ public class TestJavaFunctions extends TestBase {
         Connection c = newConnection();
         Statement  s = c.createStatement();
 
-        s.executeUpdate("DROP FUNCTION TESTQUERY IF EXISTS");
+        s.executeUpdate("DROP FUNCTION TEST_QUERY IF EXISTS");
+        s.executeUpdate("DROP FUNCTION TEST_CUSTOM_RESULT IF EXISTS");
         s.executeUpdate("DROP TABLE T IF EXISTS");
         s.executeUpdate("CREATE TABLE T(C VARCHAR(20), I INT)");
         s.executeUpdate("INSERT INTO T VALUES 'Thames', 10");
@@ -64,22 +77,54 @@ public class TestJavaFunctions extends TestBase {
         s.executeUpdate("INSERT INTO T VALUES 'Brent', 14");
         s.executeUpdate("INSERT INTO T VALUES 'Westbourne', 16");
         s.executeUpdate(
-            "CREATE FUNCTION TESTQUERY(INT) RETURNS TABLE(N VARCHAR(20), I INT) "
+            "CREATE FUNCTION TEST_QUERY(INT) RETURNS TABLE(N VARCHAR(20), I INT) "
             + " READS SQL DATA LANGUAGE JAVA EXTERNAL NAME 'CLASSPATH:org.hsqldb.test.TestJavaFunctions.getQueryResult'");
+        s.executeUpdate(
+            "CREATE FUNCTION TEST_CUSTOM_RESULT(BIGINT, BIGINT) RETURNS TABLE(I BIGINT, N VARBINARY(1000)) "
+            + " READS SQL DATA LANGUAGE JAVA EXTERNAL NAME 'CLASSPATH:org.hsqldb.test.TestJavaFunctions.getCustomResult'");
+        s.executeUpdate("CHECKPOINT");
         c.close();
     }
 
     public void testOne() throws SQLException {
 
         Connection        c = newConnection();
-        CallableStatement s = c.prepareCall("CALL TESTQUERY(6)");
+        CallableStatement s = c.prepareCall("CALL TEST_QUERY(16)");
 
         s.execute();
 
         ResultSet r = s.getResultSet();
 
         while (r.next()) {
-            r.next();
+            String temp = "" + r.getInt(2) + " " + r.getString(1);
+
+            System.out.println(temp);
+        }
+
+        s = c.prepareCall("CALL TEST_CUSTOM_RESULT(6, 19)");
+
+        s.execute();
+
+        r = s.getResultSet();
+
+        while (r.next()) {
+            String temp =
+                "" + r.getLong(1) + " "
+                + org.hsqldb.lib.StringConverter.byteArrayToSQLHexString(
+                    r.getBytes(2));
+
+            System.out.println(temp);
+        }
+
+        s = c.prepareCall("CALL TEST_CUSTOM_RESULT(6, 1900)");
+
+        try {
+            s.execute();
+            r = s.getResultSet();
+
+            fail("exception not thrown");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
         c.close();
@@ -93,5 +138,59 @@ public class TestJavaFunctions extends TestBase {
         Statement st = connection.createStatement();
 
         return st.executeQuery("SELECT * FROM T WHERE I < " + i);
+    }
+
+    private static Result newTwoColumnResult() {
+
+        Type[] types = new Type[2];
+
+        types[0] = Type.SQL_BIGINT;
+        types[1] = Type.SQL_VARBINARY_DEFAULT;
+
+        ResultMetaData  meta = ResultMetaData.newSimpleResultMetaData(types);
+        RowSetNavigator navigator = new RowSetNavigatorClient();
+        Result          result    = Result.newDataResult(meta);
+
+        result.setNavigator(navigator);
+
+        return result;
+    }
+
+    public static ResultSet getCustomResult(Connection connection, long start,
+            long end) throws SQLException {
+
+        Result result = newTwoColumnResult();
+
+        if (end < start) {
+            long temp = start;
+
+            start = end;
+            end   = temp;
+        }
+
+        if (end > 1000) {
+            throw org.hsqldb.jdbc.Util.invalidArgument(
+                "value larger than 100");
+        }
+
+        if (end > start + 100) {
+            end = start + 100;
+        }
+
+
+        for (long i = start; i < end; i++) {
+            Object[] row = new Object[2];
+
+            row[0] = Long.valueOf(i);
+            row[1] = new BinaryData(BigInteger.valueOf(i).toByteArray(),
+                                    false);
+
+            result.navigator.add(row);
+        }
+
+        result.navigator.reset();
+
+        return new JDBCResultSet((JDBCConnection) connection, null, result,
+                                 result.metaData);
     }
 }
