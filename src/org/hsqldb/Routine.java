0bb0e7f9f967b6b84ce022f085c9234d7280b52f
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2010-10-12 10:30:58 +0000

    core code updates
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@3811 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/Routine.java
+++ src/org/hsqldb/Routine.java
@@ -203,7 +203,7 @@ public class Routine implements SchemaObject, Cloneable {
         sb.append(Tokens.T_ALTER).append(' ').append(Tokens.T_SPECIFIC);
         sb.append(' ').append(Tokens.T_ROUTINE).append(' ');
         sb.append(specificName.getSchemaQualifiedStatementName());
-        sb.append(' ').append(Tokens.T_SET).append(' ').append(Tokens.T_BODY);
+        sb.append(' ').append(Tokens.T_BODY);
         sb.append(' ').append(statement.getSQL());
 
         return sb.toString();
