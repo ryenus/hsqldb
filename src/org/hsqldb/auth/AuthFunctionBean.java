af29ea524002ed58f3fc44b87a91dcd2a78f202f
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2010-11-04 01:14:44 +0000

    Renamed class AuthTriggerBean to AuthFunctionBean. This breaks build.  Need to update code in AuthBeanMultiplexer and unit tests accordingly.
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@3852 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/auth/AuthFunctionBean.java
+++ src/org/hsqldb/auth/AuthFunctionBean.java
@@ -1,13 +1,13 @@
 package org.hsqldb.auth;
 
 /**
- * N.b. AuthTriggerBeans are NOT directly usable as HyperSQL Triggers, they are
+ * N.b. AuthFunctionBeans are NOT directly usable as HyperSQL Triggers, they are
  * POJO beans to be managed by AuthBeanMultiplexer (which is a HyperSQL
  * Trigger).
  *
  * @see AuthBeanMultiplexer for how these beans are used.
  */
-public interface AuthTriggerBean {
+public interface AuthFunctionBean {
     /**
      * Return a list of authorized roles or null to indicate that the
      * implementation does not intend to produce a specific role list but only
@@ -16,10 +16,10 @@ public interface AuthTriggerBean {
      * that the user should not be granted any roles.
      *
      * @throws Exception If user should not be allowed access to the specified
-     *         database.  Other registed AuthTriggerBeans will not be attempted.
+     *         database.  Other registed AuthFunctionBeans will not be attempted.
      * @throws RuntimeException Upon system problem.  The exception will be
      *         logged to the HyperSQL application logger and other registered
-     *         AuthTriggerBeans (if any) will be attempted.
+     *         AuthFunctionBeans (if any) will be attempted.
      * @return null or String[] according to the contract of HyperSQL
      *         authentication function contract, except that the role/schema
      *         list is returned as a String[] instead of a java.sql.Array.
