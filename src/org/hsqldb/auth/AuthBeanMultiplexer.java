5d9a6c219e3ea35ed4ec61baf680444a1cebae91
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2010-11-04 01:21:32 +0000

    Finished rename of AuthTriggerBean class and related methods to AuthFunctionBean
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@3853 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/auth/AuthBeanMultiplexer.java
+++ src/org/hsqldb/auth/AuthBeanMultiplexer.java
@@ -11,7 +11,7 @@ import org.hsqldb.types.Type;
 /**
  * This class provides a method which can be used directly as a HyperSQL static
  * Java function method.
- * Manages a set of AuthTriggerBean implementations
+ * Manages a set of AuthFunctionBean implementations
  */
 public class AuthBeanMultiplexer {
     /**
@@ -26,17 +26,17 @@ public class AuthBeanMultiplexer {
     private static AuthBeanMultiplexer singleton = new AuthBeanMultiplexer();
 
     /**
-     * @see #setAuthTriggerBeans(List)
+     * @see #setAuthFunctionBeans(List)
      */
-    private static Map<String, List<AuthTriggerBean>> beans =
-            new HashMap<String, List<AuthTriggerBean>>();
+    private static Map<String, List<AuthFunctionBean>> beans =
+            new HashMap<String, List<AuthFunctionBean>>();
 
     public static AuthBeanMultiplexer getSingleton() {
         return singleton;
     }
 
     /**
-     * Clear the set of AuthTriggerBeans
+     * Clear the set of AuthFunctionBeans
      */
     public void clear() {
         AuthBeanMultiplexer.beans.clear();
@@ -44,7 +44,7 @@ public class AuthBeanMultiplexer {
 
     /**
      * Primary purpose of this class is to manage this static map.
-     * From dbNames to ordered-lists-of-AuthTriggerBeans.
+     * From dbNames to ordered-lists-of-AuthFunctionBeans.
      * This is not an "adder" function, but a "setter" function, so do not use
      * this to add to a partial set, but to assign the entire set.
      * <P>
@@ -52,95 +52,96 @@ public class AuthBeanMultiplexer {
      * issues.
      * </P>
      */
-    public void setAuthTriggerBeans(
-            Map<String, List<AuthTriggerBean>> authTriggerBeanMap) {
+    public void setAuthFunctionBeans(
+            Map<String, List<AuthFunctionBean>> authFunctionBeanMap) {
         if (AuthBeanMultiplexer.beans.size() > 0)
             throw new IllegalStateException(
-                    "Use setAuthTriggerBeans(Map) only when the set is empty");
-        AuthBeanMultiplexer.beans.putAll(authTriggerBeanMap);
+                    "Use setAuthFunctionBeans(Map) only when the set is empty");
+        AuthBeanMultiplexer.beans.putAll(authFunctionBeanMap);
     }
 
     /**
      * This is not an "adder" function, but a "setter" function for the
      * specified dbName , so do not use this to add to a database's
-     * TriggerBeans, but to assign the entire list for that database.
+     * FunctionBeans, but to assign the entire list for that database.
      * <P>
      * The given entries are copied, to limit side-effects and concurrency
      * issues.
      * </P> <P>
-     * Use this method instead of setAuthorTriggerBean(String, AuthTriggerBean)
+     * Use this method instead of setAuthFunctionBean(String, AuthFunctionBean)
      * in order to set up multiple authenticators for a single database for
      * redundancy purposes.
      * </P>
      *
-     * @see setAuthTriggerBeans(Map)
-     * @see setAuthTriggerBean(String, AuthTriggerBean)
+     * @see setAuthFunctionBeans(Map)
+     * @see setAuthFunctionBean(String, AuthFunctionBean)
      */
-    public void setAuthTriggerBeans(String dbName,
-            List<AuthTriggerBean> authTriggerBeans) {
-        List<AuthTriggerBean> dbsBeans = AuthBeanMultiplexer.beans.get(dbName);
+    public void setAuthFunctionBeans(String dbName,
+            List<AuthFunctionBean> authFunctionBeans) {
+        List<AuthFunctionBean> dbsBeans = AuthBeanMultiplexer.beans.get(dbName);
         if (dbsBeans == null) {
-            dbsBeans = new ArrayList<AuthTriggerBean>();
+            dbsBeans = new ArrayList<AuthFunctionBean>();
             AuthBeanMultiplexer.beans.put(dbName, dbsBeans);
         } else {
             if (dbsBeans.size() > 0)
                 throw new IllegalStateException(
-                        "Use setAuthTriggerBeans(String, List) only when the "
-                        + "db's AuthTriggerBean list is empty");
+                        "Use setAuthFunctionBeans(String, List) only when the "
+                        + "db's AuthFunctionBean list is empty");
         }
-        dbsBeans.addAll(authTriggerBeans);
+        dbsBeans.addAll(authFunctionBeans);
     }
 
     /**
      * This is not an "adder" function, but a "setter" function for the
      * specified dbName , so do not use this to add to a database's
-     * TriggerBeans, but to assign ths single given AuthTriggerBean as the
+     * FunctionBeans, but to assign ths single given AuthFunctionBean as the
      * specified database's authenticator.
      * <P>
      * To set up multiple authenticators for a single database for redundancy
-     * purposes, use the method setAuthorTriggerBeans(String, List) instead.
+     * purposes, use the method setAuthFunctionBeans(String, List) instead.
      * </P>
      *
-     * @see setAuthTriggerBeans(String, List)
+     * @see setAuthFunctionBeans(String, List)
      */
-    public void setAuthTriggerBean(String dbName,
-            AuthTriggerBean authTriggerBean) {
-        setAuthTriggerBeans(dbName, Collections.singletonList(authTriggerBean));
+    public void setAuthFunctionBean(String dbName,
+            AuthFunctionBean authFunctionBean) {
+        setAuthFunctionBeans(
+                dbName, Collections.singletonList(authFunctionBean));
     }
 
     /**
      * HyperSQL Java Function Method.
      * <P>
-     * Registered AuthTriggerBeans matching the specified database and password
+     * Registered AuthFunctionBeans matching the specified database and password
      * will be tried in order.
      * <OL>
-     *   <li>If the AuthTriggerBean being tried throws a non-runtime Exception,
+     *   <li>If the AuthFunctionBean being tried throws a non-runtime Exception,
      *       then that RuntimeException is passed through (re-thrown), resulting
      *       in a SQLException for the authenticating application.
-     *   <li>If the AuthTriggerBean being tried doesn't throw anything, then
+     *   <li>If the AuthFunctionBean being tried doesn't throw anything, then
      *       the return value is passed through (returned) and HyperSQL will
      *       allow access and set roles according to HyperSQL's authentication
      *       function contract.
-     *   <LI>If the AuthTriggerBean being tried throws a RuntimeException, then
-     *       the next AuthTriggerBean in turn will be tried.
-     *       If all matching AuthTriggerBeans throw RuntimeExceptions, then the
+     *   <LI>If the AuthFunctionBean being tried throws a RuntimeException, then
+     *       the next AuthFunctionBean in turn will be tried.
+     *       If all matching AuthFunctionBeans throw RuntimeExceptions, then the
      *       first RuntimeException that was thrown will be passed through
      *       (re-thrown), resulting in a SQLException for the authenticating
      *       application.
-     *   <LI>If there are no AuthTriggerBeans registered for the specified
+     *   <LI>If there are no AuthFunctionBeans registered for the specified
      *       dbName, then this method will throw an IllegalArgumentException,
      *       resulting in a SQLException for the authenticating application.
      * </OL>
      *
      * @See HyperSQL User Guide, System Management and Deployment Issues
      *         chapter, Authentication Settings subsection.
-     * @throws IllegalArgumentException if no AuthTriggerBean has been set for
+     * @throws IllegalArgumentException if no AuthFunctionBean has been set for
      *         specified dbName.
-     * @throws RuntimeException if all matching AuthTriggerBeans threw
+     * @throws RuntimeException if all matching AuthFunctionBeans threw
      *         RuntimeExceptions.  (This indicates that no matching
-     *         AuthTriggerBean functioned properly, not that authentication was
-     *         purposefully denied by any AuthTriggerBean).
-     * @throws Exception (non-runtime).  A matching AuthTriggerBean threw this
+     *         AuthFunctionBean functioned properly, not that authentication was
+     *         purposefully denied by any AuthFunctionBean).
+     * @throws Exception (non-runtime).  A matching AuthFunctionBean threw this
      *         Exception.
      * @return Null or java.sql.Array to indicate successful authentication
      *         according to the contract for HyperSQL authentication functions.
@@ -148,7 +149,7 @@ public class AuthBeanMultiplexer {
     public static java.sql.Array authenticate(
             String database, String user, String password)
             throws Exception {
-        List<AuthTriggerBean> beanList =
+        List<AuthFunctionBean> beanList =
                 AuthBeanMultiplexer.beans.get(database);
         if (beanList == null) {
             throw new IllegalArgumentException("Database '" + database
@@ -157,7 +158,7 @@ public class AuthBeanMultiplexer {
         }
         Exception firstRTE = null;
         String[] beanRet;
-        for (AuthTriggerBean nextBean : beanList) try {
+        for (AuthFunctionBean nextBean : beanList) try {
             beanRet = nextBean.authenticate(database, user, password);
             return (beanRet == null)
                     ? null : new JDBCArrayBasic(beanRet, Type.SQL_VARCHAR);
