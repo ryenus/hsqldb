0e65fb448a388b7935372dc16b08386eb1e1adc4
Blaine Simpson <unsaved@users.sf.net>
2010-11-08 04:09:13 +0000

    Replaced the LdapBeanDriver with a more powerful program that requires no user/integrator coding
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3869 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/auth/LdapAuthBean.java
+++ trunk/src/org/hsqldb/auth/LdapAuthBean.java
@@ -32,11 +32,15 @@
 package org.hsqldb.auth;
 
 import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Hashtable;
+import java.util.Properties;
 import javax.naming.AuthenticationException;
 import javax.naming.NamingException;
 import javax.naming.Context;
@@ -391,7 +395,7 @@ public class LdapAuthBean implements AuthFunctionBean {
             }
             Attribute attribute =  attrs.get(rolesSchemaAttribute);
             int valCount = attribute.size();
-            System.err.println("#" + valCount);
+            //System.err.println("#" + valCount);
             Matcher matcher;
             for (int i = 0; i < valCount; i++) {
                 if (attribute.get(i) == null) {
@@ -440,5 +444,136 @@ public class LdapAuthBean implements AuthFunctionBean {
             throw new DenyException();
         }
         return returns.toArray(new String[0]);
+    }
+
+    /**
+     * Run this method to try and test configuration settings for LdapAuthBeans,
+     * or to troubleshoot.
+     * It purposefully does not test the Java Function or the JDBC layer at all.
+     * This program will attempt to retrieve and display the schema/roles list
+     * for the specified user and with the specified password from the LDAP
+     * server according to the specified properties.
+     * <P>
+     * Passwords typed on the command line are inherently not secure, so only
+     * use this program when the computer it is run on is secured and where
+     * your command line may not be observed, directly or indirectly, by others.
+     * </P> <P>
+     * Set the properties in a properties file to match your LDAP security and
+     * Directory Information Tree structure and use this program to check
+     * everything between the LdapAuthBean and your LDAP server.
+     * You then know the exact settings to use for an LdapAuthBean that you can
+     * plug into AuthBeanMultiplexer.
+     * </P> <P>
+     * Run with no arguments to see required syntax.
+     * </P> <P>
+     *   The property file may contain any of the following properties, which
+     *   exactly match the corresponding setter methods in this class.
+     *   <UL>
+     *     <LI>trustStore.  This is the only property without a corresponding
+     *         setter method.  Setting this property has the same effect as
+     *         setting Java system property 
+     *         <CODE>'javax.net.ssl.trustStore'<CODE>.
+     *     <LI>startTls.  Takes a boolean value according to
+     *         method java.util.Boolean.parseBoolean.
+     *     <LI>roleSchemaValuePattern.  Correponds to method
+     *         setRoleSchemaValuePatternString
+     *     <LI>ldapPort
+     *     <LI>securityMechanism
+     *     <LI>ldapHost
+     *     <LI>principalTemplate
+     *     <LI>initialContextFactory
+     *     <LI>saslRealm
+     *     <LI>parentDn
+     *     <LI>rdnAttribute
+     *     <LI>rolesSchemaAttribute
+     *   </UL>
+     *   Tokens like ${this} will not be expanded to system property values,
+     *   and your bean will get the values exactly as you type them in.
+     * </P> <P>
+     * The file sample/ldap-exerciser.properties in the HyperSQL distribution
+     * may be used as a template or example.
+     * </P>
+     */
+    public static void main(String[] sa) throws IOException {
+        if (sa.length != 3) {
+            throw new IllegalArgumentException(
+                    "SYNTAX:  java " + AuthBeanMultiplexer.class.getName()
+                    + " path/to/file.properties <USERNAME> <PASSWORD>");
+        }
+        File file = new File(sa[0]);
+        if (!file.isFile()) {
+            throw new IllegalArgumentException(
+                    "Not a file: " + file.getAbsolutePath());
+        }
+        Properties p = new Properties();
+        p.load(new FileInputStream(file));
+        String trustStore = p.getProperty("trustStore");
+        String startTlsString = p.getProperty("startTls");
+        String ldapPortString = p.getProperty("ldapPort");
+        String roleSchemaValuePatternString =
+                p.getProperty("roleSchemaValuePattern");
+        String securityMechanism = p.getProperty("securityMechanism");
+        String ldapHost = p.getProperty("ldapHost");
+        String principalTemplate = p.getProperty("principalTemplate");
+        String initialContextFactory = p.getProperty("initialContextFactory");
+        String saslRealm = p.getProperty("saslRealm");
+        String parentDn = p.getProperty("parentDn");
+        String rdnAttribute = p.getProperty("rdnAttribute");
+        String rolesSchemaAttribute = p.getProperty("rolesSchemaAttribute");
+        if (trustStore != null) {
+            if (!(new File(trustStore)).isFile()) {
+                throw new IllegalArgumentException(
+                        "Specified trust store is not a file: " + trustStore);
+            }
+            System.setProperty("javax.net.ssl.trustStore", trustStore);
+        }
+        LdapAuthBean bean = new LdapAuthBean();
+        if (startTlsString != null) {
+            bean.setStartTls(Boolean.parseBoolean(startTlsString));
+        }
+        if (ldapPortString != null) {
+            bean.setLdapPort(Integer.parseInt(ldapPortString));
+        }
+        if (roleSchemaValuePatternString != null) {
+            bean.setRoleSchemaValuePatternString(roleSchemaValuePatternString);
+        }
+        if (securityMechanism != null) {
+            bean.setSecurityMechanism(securityMechanism);
+        }
+        if (ldapHost != null) {
+            bean.setLdapHost(ldapHost);
+        }
+        if (principalTemplate != null) {
+            bean.setPrincipalTemplate(principalTemplate);
+        }
+        if (initialContextFactory != null) {
+            bean.setInitialContextFactory(initialContextFactory);
+        }
+        if (saslRealm != null) {
+            bean.setSaslRealm(saslRealm);
+        }
+        if (parentDn != null) {
+            bean.setParentDn(parentDn);
+        }
+        if (rdnAttribute != null) {
+            bean.setRdnAttribute(rdnAttribute);
+        }
+        if (rolesSchemaAttribute != null) {
+            bean.setRolesSchemaAttribute(rolesSchemaAttribute);
+        }
+        bean.init();
+        String[] res = null;
+        try {
+            res = bean.authenticate(sa[1], sa[2]);
+        } catch (DenyException de) {
+            System.out.println("<DENIED>");
+            return;
+        }
+        if (res == null) {
+            System.out.println("<NULL>");
+        } else {
+            System.out.println(Integer.toString(res.length)
+                    + " roles/schema: " + Arrays.toString(res));
+        }
     }
 }
