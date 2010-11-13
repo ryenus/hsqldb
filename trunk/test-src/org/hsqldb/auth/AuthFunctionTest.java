ac25a1395d7c724a2b79f1d9d4880440503c0823
Blaine Simpson <unsaved@users.sf.net>
2010-11-13 14:08:36 +0000

    Added comprehensive test for local-only accounts, incl. the ALTER USER SET LOCAL statement and default SA behavior.
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@3895 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/test-src/org/hsqldb/auth/AuthFunctionTest.java
+++ trunk/test-src/org/hsqldb/auth/AuthFunctionTest.java
@@ -327,6 +327,82 @@ public class AuthFunctionTest extends junit.framework.TestCase {
         }
     }
 
+    public void testLocalOnlyAccess() throws SQLException {
+        Statement st = null;
+        Connection authedCon = null;
+        Connection authed2Con = null;
+        Connection extraCon = null;
+        try {
+            saSt.executeUpdate("CREATE USER tloa PASSWORD 'localPassword'");
+            saSt.executeUpdate(
+                    "SET DATABASE AUTHENTICATION FUNCTION EXTERNAL NAME "
+                    + "'CLASSPATH:" + getClass().getName() + ".nullFn'");
+            try {
+                authedCon = DriverManager.getConnection(
+                        jdbcUrl, "TLOA", "unusedPassword");
+            } catch (SQLException se) {
+                fail("Pre-test with normal access-only account failed");
+            }
+            try {
+                extraCon = DriverManager.getConnection(jdbcUrl, "SA", "wrong");
+                fail("Permitted access to SA with wrong password");
+            } catch (SQLException se) {
+                // Intentionally empty.  Expect this.
+            }
+            try {
+                extraCon = DriverManager.getConnection(jdbcUrl, "SA", "");
+            } catch (SQLException se) {
+                fail("Pre-test with SA account failed");
+            }
+            saSt.executeUpdate("ALTER USER tloa SET LOCAL true");
+            try {
+                authed2Con = DriverManager.getConnection(
+                        jdbcUrl, "TLOA", "wrongPassword");
+                fail("Permitted access to local-only user with wrong password");
+            } catch (SQLException se) {
+                // Intentionally empty.  Expect this.
+            }
+            try {
+                authedCon = DriverManager.getConnection(
+                        jdbcUrl, "TLOA", "localPassword");
+            } catch (SQLException se) {
+                fail("Access to local-only account with local password failed");
+            }
+        } finally {
+            if (extraCon != null) try {
+                extraCon.rollback();
+                extraCon.close();
+            } catch (SQLException se) {
+                logger.error("Close of Extra Conn. failed:" + se);
+            } finally {
+                extraCon = null;
+            }
+            if (st != null) try {
+                st.close();
+            } catch (SQLException se) {
+                logger.error("Close of Statement failed:" + se);
+            } finally {
+                st = null;
+            }
+            if (authedCon != null) try {
+                authedCon.rollback();
+                authedCon.close();
+            } catch (SQLException se) {
+                logger.error("Close of Authed Conn. failed:" + se);
+            } finally {
+                authedCon = null;
+            }
+            if (authed2Con != null) try {
+                authed2Con.rollback();
+                authed2Con.close();
+            } catch (SQLException se) {
+                logger.error("Close of Authed Conn. #2 failed:" + se);
+            } finally {
+                authed2Con = null;
+            }
+        }
+    }
+
     public void testPullSchema() throws SQLException {
         Statement st = null;
         Connection authedCon = null;
