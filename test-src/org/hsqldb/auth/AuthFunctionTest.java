dbb644bd0e6aa1afc96793e8ea09af069477e890
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2010-12-06 02:59:55 +0000

    Added test for remote authentication for non-existent local account
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@3927 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- test-src/org/hsqldb/auth/AuthFunctionTest.java
+++ test-src/org/hsqldb/auth/AuthFunctionTest.java
@@ -314,6 +314,44 @@ public class AuthFunctionTest extends junit.framework.TestCase {
         }
     }
 
+    public void testVirtualAccount() throws SQLException {
+        Statement st = null;
+        Connection authedCon = null;
+        try {
+            saSt.executeUpdate(
+                    "SET DATABASE AUTHENTICATION FUNCTION EXTERNAL NAME "
+                    + "'CLASSPATH:"
+                    + AuthFunctionUtils.class.getName() + ".nullFn'");
+            saSt.executeUpdate("GRANT ALL ON t1 TO public");
+            try {
+                authedCon = DriverManager.getConnection(
+                        jdbcUrl, "VIRTUALUSER", "unusedPassword");
+            } catch (SQLException se) {
+                fail("Failed to grant access to virtual user");
+            }
+            st = authedCon.createStatement();
+            assertFalse("Virtual user failed to write public-write table",
+                    AuthFunctionUtils.updateDoesThrow(
+                    st, "INSERT INTO s2t1 VALUES(2)"));
+        } finally {
+            if (st != null) try {
+                st.close();
+            } catch (SQLException se) {
+                logger.error("Close of Statement failed:" + se);
+            } finally {
+                st = null;
+            }
+            if (authedCon != null) try {
+                authedCon.rollback();
+                authedCon.close();
+            } catch (SQLException se) {
+                logger.error("Close of Authed Conn. failed:" + se);
+            } finally {
+                authedCon = null;
+            }
+        }
+    }
+
     public void testLocalOnlyAccess() throws SQLException {
         Statement st = null;
         Connection authedCon = null;
