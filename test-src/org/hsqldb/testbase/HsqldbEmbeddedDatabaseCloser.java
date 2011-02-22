84fdf92a941e83968ff32225df71ea4261f5e547
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-02-22 22:38:57 +0000

    test updates
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4093 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- test-src/org/hsqldb/testbase/HsqldbEmbeddedDatabaseCloser.java
+++ test-src/org/hsqldb/testbase/HsqldbEmbeddedDatabaseCloser.java
@@ -5,12 +5,12 @@ package org.hsqldb.testbase;
  * @author cboucher
  */
 public class HsqldbEmbeddedDatabaseCloser implements ConnectionFactory.EventListener {
-    
+
     private HsqldbEmbeddedDatabaseCloser() {}
 
     public static final ConnectionFactory.EventListener Instance = new HsqldbEmbeddedDatabaseCloser();
 
-    @Override
+//    @Override
     public void closedRegisteredObjects(ConnectionFactory source) {
         org.hsqldb.DatabaseManager.closeDatabases(-1);
     }
