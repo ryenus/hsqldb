bcf76ea32c18f9f09e8a66b07a717b8382742aaa
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-03-14 13:52:00 +0000

    core code updates
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4150 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/test/TestViewAsterisks.java
+++ src/org/hsqldb/test/TestViewAsterisks.java
@@ -391,7 +391,7 @@ public class TestViewAsterisks extends TestBase {
         // ................................................................
         checkViewTranslationAndContent(
             "C1", null, "SELECT * AS \"a2\" FROM ABC",
-            "SELECT PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C AS\"a2\" FROM PUBLIC.ABC",
+            "SELECT PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C AS \"a2\" FROM PUBLIC.ABC",
             new Object[][] {
             new Object[] {
                 new Integer(1), "a", "b", "c"
@@ -403,7 +403,7 @@ public class TestViewAsterisks extends TestBase {
         // ................................................................
         checkViewTranslationAndContent(
             "C2", null, "SELECT B AS \"b2\", ABC.* FROM ABC",
-            "SELECT B AS\"b2\" , PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C  FROM PUBLIC.ABC",
+            "SELECT B AS \"b2\", PUBLIC.ABC.ID,PUBLIC.ABC.A,PUBLIC.ABC.B,PUBLIC.ABC.C  FROM PUBLIC.ABC",
             new Object[][] {
             new Object[] {
                 "b", new Integer(1), "a", "b", "c"
