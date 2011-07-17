d197127759f777fd803083dd975aa581de32fbab
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-07-17 10:57:44 +0000

    core code updates - minor fixes
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4379 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/ExpressionArithmetic.java
+++ src/org/hsqldb/ExpressionArithmetic.java
@@ -491,11 +491,9 @@ public class ExpressionArithmetic extends Expression {
         }
 
         if (nodes[LEFT].isUnresolvedParam()) {
-            nodes[LEFT].dataType = Type.SQL_VARCHAR_DEFAULT;
-        }
-
-        if (nodes[RIGHT].isUnresolvedParam()) {
-            nodes[RIGHT].dataType = Type.SQL_VARCHAR_DEFAULT;
+            nodes[LEFT].dataType = getParameterType(nodes[RIGHT].dataType);
+        } else if (nodes[RIGHT].isUnresolvedParam()) {
+            nodes[RIGHT].dataType = getParameterType(nodes[LEFT].dataType);
         }
 
         if (nodes[LEFT].dataType == null) {
@@ -566,6 +564,40 @@ public class ExpressionArithmetic extends Expression {
         if (nodes[LEFT].opType == OpTypes.VALUE
                 && nodes[RIGHT].opType == OpTypes.VALUE) {
             setAsConstantValue(session);
+        }
+    }
+
+    private Type getParameterType(Type type) {
+
+        if (type == null) {
+            return null;
+        }
+
+        switch (type.typeCode) {
+
+            case Types.SQL_CHAR :
+            case Types.SQL_VARCHAR :
+                return Type.SQL_VARCHAR_DEFAULT;
+
+            case Types.SQL_CLOB :
+                return Type.SQL_CLOB;
+
+            case Types.SQL_BINARY :
+            case Types.SQL_VARBINARY :
+                return Type.SQL_VARBINARY_DEFAULT;
+
+            case Types.SQL_BLOB :
+                return Type.SQL_BLOB;
+
+            case Types.SQL_BIT :
+            case Types.SQL_BIT_VARYING :
+                return Type.SQL_BIT_VARYING_MAX_LENGTH;
+
+            case Types.SQL_ARRAY :
+                return type;
+
+            default :
+                return null;
         }
     }
 
