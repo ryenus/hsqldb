933a0f6f41c82bcde5480f5815be0dce343903c0
Blaine Simpson <unsaved@users.sf.net>
2011-10-22 16:37:30 +0000

    The out-of-the-box gradlew.bat doesn't have nearly as good error detection and halding as the out-of-the-box gradlew script. This commit adds compensating error detection and handling to our gradle-gui.cmd script.
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@4600 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/build/gradle-gui.cmd
+++ trunk/build/gradle-gui.cmd
@@ -13,12 +13,44 @@ if "%DIRNAME%" == "" set DIRNAME=\.\
 :: gradlew must be run from the HyperSQL build directory:
 cd "%DIRNAME%"
 ::cd
+
+:: Eliminate confusing problems between ERRORLEVEL state and ERRORLEVEL var
+set ERRORLEVEL=
+:: Reset ERRORLEVEL state to 0:
+ver > nul
+
+if not "%JAVA_HOME%" == "" (
+    rem Test JAVA_HOME if it is set
+    rem echo JAVA_HOME is set
+    rem echo Executing: "%JAVA_HOME%\bin\java" -version
+    "%JAVA_HOME:/=\%\bin\java" -version > nul 2>&1
+    if ERRORLEVEL 1 (
+        echo:
+        echo Please fix your JAVA_HOME env. variable.  Executable 'java' does not exist: '%JAVA_HOME:/=\%\bin\java.*'
+        echo:
+        pause
+        exit /b
+    )
+)
+if "%JAVA_HOME%" == "" (
+    rem If JAVA_HOME not set, then test for 'java' executable
+    java -version > nul 2>&1
+    if ERRORLEVEL 1 (
+        echo:
+        echo You must set env. variable 'JAVA_HOME' or put 'java' into your search path.
+        echo:
+        pause
+        exit /b
+    )
+)
+
 type gui-welcome.txt
 
 :: If there is no settings file in place, start user with our customized one:
 if not exist gradle-app.setting copy gui-initial.setting gradle-app.setting > nul
 
-.\gradlew.bat --gui %*
+call .\gradlew.bat --gui %*
+:: ERRORLEVEL will be passed through to caller with no extra work here.
 
-:: Enable following line for debugging purposes:
-::pause
+:: Give user an opportunity to see the error:
+if ERRORLEVEL 1 pause
