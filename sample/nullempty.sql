f0e1fd0f72d93f528a17f77759ef57a2a4e36187
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-10-27 18:42:00 +0000

    Added educational sample SQL script
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4651 7c7dc5f5-a22d-0410-a3af-b41755a11667

---
+++ sample/nullempty.sql
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ *
+ * This sample shows differences between null and empty strings,
+ * and ? var vs. _/~ variables.
+ */
+
+\p At startup ? is equal to empty string.  See between A and B:  A*{?}B
+* if (A*{?}B == AB)
+    \p ? is the empty string
+* end if
+
+CREATE TABLE t(i INTEGER, vc VARCHAR(20));
+INSERT INTO t VALUES(1, 'one');
+INSERT INTO t VALUES(2, 'two');
+* res ~
+SELECT * FROM t;
+\p *{?}
+\p *{res}
+* listvalues ? res
+
+INSERT INTO t VALUES (3, null);
+*res ~
+SELECT vc FROM t WHERE i = 3;
+\p *{?}
+* if (*res == **NULL)
+    \p res really is null
+* end if
+* listvalues ? res
+
+-- This will prevent SqlTool from aborting when we run a bad SQL statement:
+\c true
+*res ~
+SELECT hocus FROM pocus;
+* if (*? == **NULL)
+    \p ? really is null
+* end if
+* if (*res == **NULL)
+    \p res really is null
+* end if
+* listvalues ? res
