1883d43dab78b6b128406e99840fe964a2c96b9e
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-11-02 21:31:56 +0000

    core code updates - minor fixes
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4675 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/View.java
+++ src/org/hsqldb/View.java
@@ -282,7 +282,7 @@ public class View extends TableDerived {
                 break;
 
             case StatementTypes.UPDATE_WHERE :
-                isTriggerInsertable = false;
+                isTriggerUpdatable = false;
                 break;
 
             default :
