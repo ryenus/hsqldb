6fcfbf5e94b59fa28f67be1f0f47940f748c0d85
Blaine Simpson <unsaved@users.sf.net>
2011-11-05 01:50:17 +0000

    Updated sample scripts and Util Guide to reflect new if construct
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@4709 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/sample/nullempty.sql
+++ trunk/sample/nullempty.sql
@@ -6,9 +6,7 @@
  */
 
 \p At startup ? is equal to empty string.  See between A and B:  A*{?}B
-* if (A*{?}B == AB)
-    \p ? is the empty string
-* end if
+* if (A*{?}B == AB) \p ? is the empty string
 
 CREATE TABLE t(i INTEGER, vc VARCHAR(20));
 INSERT INTO t VALUES(1, 'one');
@@ -23,19 +21,13 @@ INSERT INTO t VALUES (3, null);
 *res ~
 SELECT vc FROM t WHERE i = 3;
 \p *{?}
-* if (*res == **NULL)
-    \p res really is null
-* end if
+* if (*res == **NULL) \p res really is null
 * listvalues ? res
 
 -- This will prevent SqlTool from aborting when we run a bad SQL statement:
 \c true
 *res ~
 SELECT hocus FROM pocus;
-* if (*? == **NULL)
-    \p ? really is null
-* end if
-* if (*res == **NULL)
-    \p res really is null
-* end if
+* if (*? == **NULL) \p ? really is null
+* if (*res == **NULL) \p res really is null
 * listvalues ? res
