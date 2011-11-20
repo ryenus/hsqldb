b0d4b32bb4075c00ff77a0c80195d92f8f38b380
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-11-20 21:18:10 +0000

    Removed swathes of text explaining legacy concern that is now gone
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4810 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- sample/csv-sample.sql
+++ sample/csv-sample.sql
@@ -14,14 +14,6 @@
 -- For applications like MS Excel, which can't import or export nulls, we have
 -- to dummy down our database empty strings to export and import as if they
 -- were nulls.
--- N.b.!!!!  With current version of SqlTool, setting any PL variable to an
--- empty string like this requires that you set Java sytem property
--- sqltool.REMOVE_EMPTY_VARS to 'false'.  This will become unnecessary soon
--- because this will become the default SqlTool behavior.
--- Until then, invoke SqlTool like:
---    java -Dsqltool.REMOVE_EMPTY_VARS=false -jar .../sqltool.jar...
--- or
---    java -Dsqltool.REMOVE_EMPTY_VARS=false org.hsqldb.cmdline.SqlTool...
 * *NULL_REP_TOKEN =
 
 -- Enable following line to quote every cell value
@@ -59,8 +51,6 @@ commit;
 -- 6. MANUALLY EXAMINE DIFFERENCES BETWEEN SOURCE AND IMPORTED DATA.
 -- See <HSQLDB_ROOT>/testrun/sqltool/csv-roundtrip.sql to see a way to make
 -- this same comparison programmatically.
--- Unset *NULL_REP_TOKEN so we can distinguish nulls from empty strings in our
--- examination.
 * - *NULL_REP_TOKEN
 \p
 \p ORIGINAL:
@@ -69,7 +59,7 @@ SELECT * FROM orig;
 \p IMPORTED:
 SELECT * FROM t;
 \p
-\p If you ran with sqltool.REMOVE_EMPTY_VARS false, then the empty string in
-\p the source table will have been translated to null in the imported data.
-\p If so, you can see that the generated CSV file represents both nulls and
+\p The empty string in the source table will have been translated to null in
+\p the imported data.
+\p You can see that the generated CSV file represents both nulls and
 \p empty strings as nothing, hence the convergence.
