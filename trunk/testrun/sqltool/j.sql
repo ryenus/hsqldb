bf6f03a8c7b576db05c360216077bedfe92ae6f7
Blaine Simpson <unsaved@users.sf.net>
2011-11-24 23:15:42 +0000

    Finalized \j unit test
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@4816 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/testrun/sqltool/j.sql
+++ trunk/testrun/sqltool/j.sql
@@ -9,15 +9,15 @@ INSERT INTO t1 VALUES(11);
 COMMIT;
 
 \j SA jdbc:hsqldb:mem:dataSource2
-SET PASSWORD sapwd;
+SET PASSWORD 'sapwd';
 CREATE TABLE t2(i int);
-INSERT INTO t2 VALUES(11);
+INSERT INTO t2 VALUES(22);
 COMMIT;
-
-\j SA sapwd jdbc:hsqldb:mem:dataSource2
-SELECT * FROM t1;
-* if (*? != 11) \q \j failed
 
 \j SA jdbc:hsqldb:mem:utst
 SELECT * FROM t1;
 * if (*? != 11) \q \j failed
+
+\j SA sapwd jdbc:hsqldb:mem:dataSource2
+SELECT * FROM t2;
+* if (*? != 22) \q \j failed
