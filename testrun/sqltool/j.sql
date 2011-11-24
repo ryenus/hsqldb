905bc8ee2f0693da5387dc5ec437435e5ee5f01a
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-11-24 22:58:40 +0000

    Completed \j unit test, I think
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4815 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- testrun/sqltool/j.sql
+++ testrun/sqltool/j.sql
@@ -4,15 +4,20 @@
  * Tests \j command
  */
 
-CREATE TABLE t(i int);
-INSERT INTO t VALUES(11);
+CREATE TABLE t1(i int);
+INSERT INTO t1 VALUES(11);
 COMMIT;
 
 \j SA jdbc:hsqldb:mem:dataSource2
 SET PASSWORD sapwd;
+CREATE TABLE t2(i int);
+INSERT INTO t2 VALUES(11);
+COMMIT;
+
+\j SA sapwd jdbc:hsqldb:mem:dataSource2
+SELECT * FROM t1;
+* if (*? != 11) \q \j failed
 
 \j SA jdbc:hsqldb:mem:utst
-SELECT * FROM t;
-* if (*? != 11)
-    \q \j failed
-* end if
+SELECT * FROM t1;
+* if (*? != 11) \q \j failed
