6bd9a1e0ea8a593f3607e8cb93186275bd7ece32
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2011-12-27 15:55:44 +0000

    More complete raw mode tests
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@4852 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- testrun/sqltool/rawmode.sql
+++ testrun/sqltool/rawmode.sql
@@ -19,3 +19,14 @@ SELECT i FROM t
 *if (*? != 42)
     \q Raw command failed
 *end if
+
+-- To change results so we can be confident of getting 1 later on.
+INSERT INTO t values (43);
+SELECT * FROM t;
+
+\.
+SELECT count(*) FROM t
+.
+:a WHERE i = 42
+;
+*if (*? != 1) \q Raw command failed
