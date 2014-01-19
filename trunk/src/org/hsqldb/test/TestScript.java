1302ec17a36b139d476a2a2cab1565ab99dfad03
Fred Toussi <fredt@users.sf.net>
2014-01-19 00:10:02 +0000

    core code updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@5328 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/test/TestScript.java
+++ trunk/src/org/hsqldb/test/TestScript.java
@@ -31,40 +31,10 @@
 
 package org.hsqldb.test;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 
 public class TestScript extends TestBase {
 
-//    String path = "TestSelf00NewJoins.txt";
-//      String path = "TestSelf01AllNewFeatures.txt";
-//    String path = "TestSelf01Aggregates.txt";
-//    String path = "TestSelf01Cascade.txt";
-      String path = "TestSelf01DateTime.txt";
-//    String path = "TestSelf01Etc.txt";
-//    String path = "TestSelf01UDAggregates.txt";
-//    String path = "TestSelf01hsqldb_schema.txt";
-//    String path = "TestSelf01WithSubquery.txt";
-//    String path = "TestSelf01RecursiveQueries.txt";
-//    String path = "TestSelf01RoutineHandlers.txt";
-//    String path = "TestSelf01TempTables.txt";
-//    String path = "TestSelfTriggers2.txt";
-//    String path = "TestSelf01ForeignKeys.txt";
-//    String path = "TestSelf01Function.txt";
-//    String path = "TestSelf01IndexUse.txt";
-//    String path = "TestSelf01LimitOpt.txt";
-//    String path = "TestSelf01Procedure.txt";
-//    String path = "TestSelf01Problems.txt";
-//    String path = "TestSelf01Subquery.txt";
-//    String path = "TestSelf01TempTables.txt";
-//    String path = "TestSelf01Triggers.txt";
-//    String path = "TestSelf02Function.txt";
-//    String path = "TestSelf02UpdatableViews.txt";
-//    String path = "TestSelf02Merge.txt";
-//    String path = "TestSelf02UpdatableViews.txt";
-//    String path = "TestSelf04UnionCorresponding.txt";
-//    String path = "TestSelf07OrderLimitNulls.txt";
-//    String path = "TestSelf06Generated.txt";
-//    String path = "TestSelf08Collations.txt";
 //    String path = "TestAny.txt";
 //    String path = "TestSelf.txt";
 //    String path = "TestSelfArrays.txt";
