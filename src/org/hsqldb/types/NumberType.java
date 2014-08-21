0c0a506b52da8edc45a44a1b73788cadfd55df92
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2014-08-21 12:37:33 +0000

    core code updates
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@5391 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/types/NumberType.java
+++ src/org/hsqldb/types/NumberType.java
@@ -1658,13 +1658,25 @@ public final class NumberType extends Type {
             throw Error.error(ErrorCode.X_42561);
         }
 
+        a = truncate(a, scale);
+        b = ((NumberType) otherType).truncate(b, otherType.scale);
+
         Object temp = divide(null, a, b);
 
+        switch (typeCode) {
+
+            case Types.SQL_REAL :
+            case Types.SQL_FLOAT :
+            case Types.SQL_DOUBLE :
+            case Types.SQL_NUMERIC :
+            case Types.SQL_DECIMAL :
+                temp = truncate(temp, 0);
+        }
+
         temp = multiply(temp, b);
-        temp = convertToDefaultType(null, temp);
         temp = subtract(session, a, temp, this);
 
-        return convertToTypeLimits(null, temp);
+        return otherType.convertToType(null, temp, this);
     }
 
     public Object absolute(Object a) {
