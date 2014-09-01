33106c074f6be561f3193b671a52b7725c332798
Fred Toussi <fredt@users.sf.net>
2014-09-01 18:53:55 +0000

    core code updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@5394 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/lib/Collection.java
+++ trunk/src/org/hsqldb/lib/Collection.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001-2011, The HSQL Development Group
+/* Copyright (c) 2001-2014, The HSQL Development Group
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,7 +34,7 @@ package org.hsqldb.lib;
 /**
  *
  * @author Fred Toussi (fredt@users dot sourceforge.net)
- * @version 1.7.2
+ * @version 2.3.3
  * @since 1.7.2
  */
 public interface Collection {
