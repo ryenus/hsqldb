67cfcfb7b35cd202679b1971d067561ff6ad76c6
fredt <fredt@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2015-03-05 15:49:18 +0000

    core code updates
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@5431 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/TransactionManager2PL.java
+++ src/org/hsqldb/TransactionManager2PL.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001-2014, The HSQL Development Group
+/* Copyright (c) 2001-2015, The HSQL Development Group
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -121,15 +121,15 @@ implements TransactionManager {
 
     public void rollback(Session session) {
 
-        session.abortTransaction        = false;
-        session.actionTimestamp         = getNextGlobalChangeTimestamp();
-        session.transactionEndTimestamp = session.actionTimestamp;
-
-        rollbackPartial(session, 0, session.transactionTimestamp);
-        endTransaction(session);
         writeLock.lock();
 
         try {
+            session.abortTransaction        = false;
+            session.actionTimestamp         = getNextGlobalChangeTimestamp();
+            session.transactionEndTimestamp = session.actionTimestamp;
+
+            rollbackPartial(session, 0, session.transactionTimestamp);
+            endTransaction(session);
             endTransactionTPL(session);
         } finally {
             writeLock.unlock();
