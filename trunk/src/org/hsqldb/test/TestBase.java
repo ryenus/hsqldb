367cbe66c39d6ff63ba6caabcf780794644284b8
Fred Toussi <fredt@users.sf.net>
2015-03-15 01:23:55 +0000

    core code updates
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@5438 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/test/TestBase.java
+++ trunk/src/org/hsqldb/test/TestBase.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001-2014, The HSQL Development Group
+/* Copyright (c) 2001-2015, The HSQL Development Group
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -36,9 +36,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.hsqldb.Database;
 import org.hsqldb.server.Server;
 import org.hsqldb.server.WebServer;
-
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 
@@ -144,7 +144,7 @@ public abstract class TestBase extends TestCase {
     protected void tearDown() {
 
         if (isNetwork && !isServlet) {
-            server.stop();
+            server.shutdownWithCatalogs(Database.CLOSEMODE_IMMEDIATELY);
 
             server = null;
         }
