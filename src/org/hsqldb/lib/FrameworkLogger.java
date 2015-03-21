55590141eed9ccecdde32ea8cf9b9204ae1a6fb0
unsaved <unsaved@7c7dc5f5-a22d-0410-a3af-b41755a11667>
2015-03-21 16:12:07 +0000

    Implemented support for Log4j v2
    git-svn-id: https://svn.code.sf.net/p/hsqldb/svn/base/trunk@5442 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/lib/FrameworkLogger.java
+++ src/org/hsqldb/lib/FrameworkLogger.java
@@ -117,10 +117,11 @@ public class FrameworkLogger {
             loggerInstances.keySet()).toString();
     }
 
-    static private Map    loggerInstances  = new HashMap();
-    static private Map    jdkToLog4jLevels = new HashMap();
-    static private Method log4jGetLogger;
-    static private Method log4jLogMethod;
+    static private Map     loggerInstances  = new HashMap();
+    static private Map     jdkToLog4jLevels = new HashMap();
+    static private Method  log4jGetLogger;
+    static private Method  log4jLogMethod;
+    static private boolean callerFqcnAvailable = false;
     private Object        log4jLogger;
     private Logger        jdkLogger;
 
@@ -208,11 +209,67 @@ public class FrameworkLogger {
 
         log4jGetLogger   = null;
         log4jLogMethod   = null;
+        callerFqcnAvailable = false;
 
         // Precedence:
         //   1) Use log4j v2 if available and class initialization succeeds
         //   2) Use log4j v1 if available and class initialization succeeds
         //   3) JUL
+
+        try {
+            // log4j v2 available?
+            log4jLoggerClass = Class.forName(
+                    "org.apache.logging.log4j.Logger");
+            log4jManagerClass = Class.forName(
+                    "org.apache.logging.log4j.LogManager");
+        } catch (Exception e) {
+
+            // The class will only load successfully if Log4j v2 thinks it is
+            // in usable state.
+            // Intentionally empty.
+        }
+
+        // Attempt to configure log4j v2
+        if (log4jLoggerClass != null) {
+            try {
+                populateJdkToLog4jLevels("org.apache.logging.log4j.Level");
+
+                log4jLogMethod = log4jLoggerClass.getMethod("log",
+                        new Class[] {
+                    Class.forName("org.apache.logging.log4j.Level"),
+                    Object.class, Throwable.class
+                });
+                log4jGetLogger = log4jManagerClass.getMethod("getLogger",
+                        new Class[]{ String.class });
+                // This last object is what we toggle on to generate either
+                // Log4j or Jdk Logger objects (to wrap).
+
+                return;    // Success for Log4j v2
+            } catch (Exception e) {
+                // This is an unexpected  problem, because our Log4j try block will
+                // only be attempted if Log4j itself initialized (even if it
+                // successfully initialized with warnings due to bad config).
+                try {
+                    System.err.println(
+                        "<clinit> failure "
+                        + "instantiating configured Log4j v2 system: " + e);
+
+                    // It's possible we don't have write access to System.err.
+                } catch (Throwable t) {
+
+                    // Intentionally empty.  We tried our best to report problem,
+                    // but don't want to throw and prevent JUL from working.
+                }
+            }
+        }
+
+        // Reset
+        log4jLoggerClass = null;
+        log4jManagerClass = null;
+        log4jLogMethod   = null;
+        log4jGetLogger   = null;
+        jdkToLog4jLevels.clear();
+
         try {
             // log4j v1 available?
             log4jLoggerClass = Class.forName("org.apache.log4j.Logger");
@@ -238,6 +295,7 @@ public class FrameworkLogger {
                         new Class[]{ String.class });
                 // This last object is what we toggle on to generate either
                 // Log4j or Jdk Logger objects (to wrap).
+                callerFqcnAvailable = true;
 
                 return;    // Success for Log4j v1
             } catch (Exception e) {
@@ -263,6 +321,7 @@ public class FrameworkLogger {
         log4jManagerClass = null;
         log4jLogMethod   = null;
         log4jGetLogger   = null;
+        callerFqcnAvailable = false;
         jdkToLog4jLevels.clear();
 
         String propVal = System.getProperty("hsqldb.reconfig_logging");
@@ -448,10 +507,13 @@ public class FrameworkLogger {
             }
         } else {
             try {
-                log4jLogMethod.invoke(log4jLogger, new Object[] {
-                    skipClass.getName(), jdkToLog4jLevels.get(level), message,
-                    t
-                });
+                log4jLogMethod.invoke(log4jLogger, callerFqcnAvailable
+                         ? new Object[] {
+                            skipClass.getName(), jdkToLog4jLevels.get(level),
+                            message, t}
+                         : new Object[] {
+                            jdkToLog4jLevels.get(level), message, t}
+                );
             } catch (Exception e) {
                 throw new RuntimeException(
                     "Logging failed when attempting to log: " + message, e);
@@ -476,10 +538,13 @@ public class FrameworkLogger {
             jdkLogger.logp(level, c, m, message);
         } else {
             try {
-                log4jLogMethod.invoke(log4jLogger, new Object[] {
-                    FrameworkLogger.class.getName(),
-                    jdkToLog4jLevels.get(level), message, null
-                });
+                log4jLogMethod.invoke(log4jLogger, callerFqcnAvailable
+                        ? new Object[] {
+                           FrameworkLogger.class.getName(),
+                           jdkToLog4jLevels.get(level), message, null}
+                        : new Object[] {
+                           jdkToLog4jLevels.get(level), message, null}
+                );
 
                 // Test where SqlFile correct here.
             } catch (Exception e) {
