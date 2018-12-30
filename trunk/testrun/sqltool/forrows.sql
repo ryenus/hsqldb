034d0e8535ba03d00799f5f52dcb00ac26a8f21e
Blaine Simpson <unsaved@users.sf.net>
2018-12-30 18:29:08 +0000

    Added SqlTools testcase for new PL sys var *ROWS
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@5895 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/testrun/sqltool/forrows.sql
+++ trunk/testrun/sqltool/forrows.sql
@@ -10,20 +10,28 @@ insert into t values(2, 'two');
 insert into t values(null, 'three');
 insert into t values(4, null);
 
+* *DSV_ROW_DELIM = @
+* if (**ROWS != **NULL) \q **ROWS not null before any forrows
 * RES={
 * EXPECTED={ Q1 1|one 1,(one) Q1 2|two 2,(two) Q1 [null]|three ,(three) Q1 4|[null] 4,() }
 *forrows   INT   VCHAR  
 SELECT * FROM t;
     * RES=*{RES} Q1 *{*ROW}
     * RES=*{RES} *{:INT},(*{:VCHAR})
+    * if (**ROWS != **NULL) \q **ROWS not null inside first forrows
 *end forrows
 * RES=*{RES} }
 * if (*RES != *EXPECTED) \q [*{RES}] != [*{EXPECTED}]
+
+* if (**ROWS == **NULL) \q **ROWS was not populated by first forrows
+* EXPECTED=1|one@2|two@[null]|three@4|[null
+* if (**ROWS != *EXPECTED) \q *ROWS: [*{*ROWS}] != [*{EXPECTED}]
 
 * RES={
 * EXPECTED={ Q2 1|one Q2 2|two Q2 [null]|three Q2 4|[null] }
 *forrows   
 SELECT * FROM t;
+    * if (**ROWS != **NULL) \q **ROWS not null inside second forrows
     * RES=*{RES} Q2 *{*ROW}
 *end forrows
 * RES=*{RES} }
