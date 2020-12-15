4821b23d349d3aacf35166e12d64ad294b7d1716
Blaine Simpson <unsaved@users.sf.net>
2020-12-15 16:56:14 +0000

    Optimized sqltool module descriptor based on jdeps generation
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base/trunk@6230 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/modules/org.hsqldb.sqltool/module-info.java
+++ src/modules/org.hsqldb.sqltool/module-info.java
@@ -1,5 +1,9 @@
 module org.hsqldb.sqltool {
+    requires transitive java.logging;
     requires transitive java.sql;
-    exports org.hsqldb.cmdline.utilclone;
+
     exports org.hsqldb.cmdline;
+    exports org.hsqldb.cmdline.libclone;
+    exports org.hsqldb.sqltool;
+    exports org.hsqldb.cmdline.utilclone;
 }
