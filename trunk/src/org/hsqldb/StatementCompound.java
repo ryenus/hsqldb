3bdf436c90829b2c0a12758500de12229fa5eaf4
Fred Toussi <fredt@users.sf.net>
2021-02-03 22:24:57 +0000

    core code updates - routine nested variables
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@6286 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/StatementCompound.java
+++ trunk/src/org/hsqldb/StatementCompound.java
@@ -950,12 +950,10 @@ public class StatementCompound extends Statement implements RangeGroup {
     private void initialiseVariables(Session session) {
 
         Object[] vars   = session.sessionContext.routineVariables;
-        int      offset = parent == null ? 0
-                                         : parent.scopeVariables.size();
 
         for (int i = 0; i < variables.length; i++) {
             try {
-                vars[offset + i] = variables[i].getDefaultValue(session);
+                vars[variablesOffset + i] = variables[i].getDefaultValue(session);
             } catch (HsqlException e) {}
         }
     }
