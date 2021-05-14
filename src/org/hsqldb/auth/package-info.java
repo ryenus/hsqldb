4f83816b1ea6b4e67e116f21c8c065a961985410
Fred Toussi <fredt@users.sf.net>
2021-05-14 21:51:15 +0000

    build - javadoc
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base/trunk@6340 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/auth/package-info.java
+++ src/org/hsqldb/auth/package-info.java
@@ -29,36 +29,7 @@
  */
 
 
-package org.hsqldb.auth;
-
 /**
- * AuthFunctionBeans are NOT directly usable as HyperSQL Authentication
- * Function methods, they are POJO beans to be managed by AuthBeanMultiplexer
- * (which does have a real HyperSQL Authentication Function static method).
- *
- * @see AuthBeanMultiplexer for how these beans are used.
- * @author Blaine Simpson (blaine dot simpson at admc dot com)
- * @since 2.0.1
+ * Contains the HyperSQL utility classes for managing external user authentication.
  */
-public interface AuthFunctionBean {
-
-    /**
-     * Return a list of authorized roles or null to indicate that the
-     * implementation does not intend to produce a specific role list but only
-     * to indicate whether to allow access or not.
-     * A return value of String[0] is different from returning null, and means
-     * that the user should not be granted any roles.
-     *
-     * @param userName String
-     * @param password String
-     * @throws Exception If user should not be allowed access to the specified
-     *         database.  Other registered AuthFunctionBeans will not be attempted.
-     * @throws RuntimeException Upon system problem.  The exception will be
-     *         logged to the HyperSQL application logger and other registered
-     *         AuthFunctionBeans (if any) will be attempted.
-     * @return null or String[] according to the contract of HyperSQL
-     *         authentication function contract, except that the role/schema
-     *         list is returned as a String[] instead of a java.sql.Array.
-     */
-    String[] authenticate(String userName, String password) throws Exception;
-}
+package org.hsqldb.auth;
