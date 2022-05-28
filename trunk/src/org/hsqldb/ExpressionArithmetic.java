fd0994d73d5dba7de73a913eb0fec4eb539d95c6
Fred Toussi <fredt@users.sf.net>
2022-05-28 17:37:26 +0000

    core code updates - fix for issue #1635 jdk.unsupported
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base@6533 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- trunk/src/org/hsqldb/ExpressionArithmetic.java
+++ trunk/src/org/hsqldb/ExpressionArithmetic.java
@@ -43,7 +43,7 @@ import org.hsqldb.types.Types;
  *
  * @author Campbell Burnet (campbell-burnet@users dot sourceforge.net)
  * @author Fred Toussi (fredt@users dot sourceforge.net)
- * @version 2.4.0
+ * @version 2.7.0
  * @since 1.9.0
  */
 public class ExpressionArithmetic extends Expression {
