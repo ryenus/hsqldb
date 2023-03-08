339f0e78cee005c2b3ce685c24c95cfc48f5864f
Fred Toussi <fredt@users.sf.net>
2023-03-08 12:54:19 +0000

    version update - to 2.7.2
    git-svn-id: http://svn.code.sf.net/p/hsqldb/svn/base/trunk@6650 7c7dc5f5-a22d-0410-a3af-b41755a11667

--- src/org/hsqldb/RangeVariable.java
+++ src/org/hsqldb/RangeVariable.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2001-2022, The HSQL Development Group
+/* Copyright (c) 2001-2023, The HSQL Development Group
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -55,7 +55,7 @@ import org.hsqldb.types.Type;
  * Metadata for range variables, including conditions.
  *
  * @author Fred Toussi (fredt@users dot sourceforge.net)
- * @version 2.6.1
+ * @version 2.7.2
  * @since 1.9.0
  */
 public class RangeVariable {
